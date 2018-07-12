d34037e26f273bf2151e1b27486c8f6245e0e757
arisupriatna14 <arisupriatna703@gmail.com>
2018-07-12 16:51:56 +0700

    add release 1. (masih ada bug)

--- battleship.js
+++ battleship.js
@@ -1,24 +1,24 @@
 const ship = [
   {
-    name: "<_A_>",
+    name: "A",
     size: 5,
     direction: "down",
     pos: [[1, 3], [1, 4], [1, 5], [1, 6], [1, 7]]
   },
   {
-    name: "<_B_>",
+    name: "B",
     size: 4,
     direction: "right",
     pos: [[3, 9], [4, 9], [5, 9], [6, 9]]
   },
   {
-    name: "<_C_>",
+    name: "C",
     size: 3,
     direction: "left",
     pos: [[6, 3], [7, 3], [8, 3]]
   },
   {
-    name: "<_D_>",
+    name: "D",
     size: 2,
     direction: "up",
     pos: [[3, 1], [3, 2]]
@@ -54,6 +54,7 @@ function generateShips() {
   return board;
 }
 
+/* tembaKKapal release 0 (static)
 function tembakKapal(titikCoordinat) {
   const kumpulanTitikTembak = titikCoordinat;
   let kumpulanTitikTembakArr = [];
@@ -75,5 +76,97 @@ function tembakKapal(titikCoordinat) {
   }
 
   console.log(boardGame)
+} */
+
+function randomCoordinat() {
+  return Math.floor(Math.random() * 9);
 }
-tembakKapal(["03", "14", "25", "16", "17", "39", "49", "59", "69", "63"]);
+
+function shipDirection () {
+  var coorX = randomCoordinat()
+  var coorY = randomCoordinat()
+  const board = gameBoard()  
+  
+  for (let i = 0; i < ship.length; i++) {
+    switch (ship[i].direction) {
+      case 'down': 
+        coorX = randomCoordinat()
+        coorY = randomCoordinat()
+        if (board[coorX][coorY] === '' && board[coorX][coorY] != undefined) {
+          if (board.length <= 10 && board[i].length <= 10) {
+            for (let j = 0; j < ship[i].size; j++) {
+              board[coorX + j][coorY] = ship[i].name
+            }
+          }
+        } else {
+          coorX = randomCoordinat()
+          coorY = randomCoordinat()
+        }
+      break
+      case 'right': 
+        coorX = randomCoordinat()
+        coorY = randomCoordinat()
+        if (board[coorX][coorY] === '' && board[coorX][coorY] != undefined) {
+          for (let j = 0; j < ship[i].size; j++) {
+            board[coorX][coorY + j] = ship[i].name
+          }
+        } else {
+          coorX = randomCoordinat()
+          coorY = randomCoordinat()
+        }
+      break
+      case 'left': 
+        coorX = randomCoordinat()
+        coorY = randomCoordinat()
+        if (board[coorX][coorY] === '' && board[coorX][coorY] != undefined) {
+          for (let j = 0; j < ship[i].size; j++) {
+            board[coorX][coorY-j] = ship[i].name
+          }
+        } else {
+          coorX = randomCoordinat()
+          coorY = randomCoordinat()
+        }
+      break
+      case 'up': 
+        coorX = randomCoordinat()
+        coorY = randomCoordinat()
+        if (board[coorX][coorY] === '' && board[coorX][coorY] != undefined) {
+          for (let j = 0; j < ship[i].size; j++) {
+            board[coorX-j][coorY] = ship[i].name
+          }
+        } else {
+          coorX = randomCoordinat()
+          coorY = randomCoordinat()
+        }
+      break
+    }
+  }
+  // console.log(board)
+  return board
+}
+// shipDirection()
+
+function tembakKapal(titikCoordinat) {
+  const kumpulanTitikTembak = titikCoordinat;
+  let kumpulanTitikTembakArr = [];
+  for (let i = 0; i < kumpulanTitikTembak.length; i++) {
+    var titikCoordinatSplit = kumpulanTitikTembak[i].split("");
+    kumpulanTitikTembakArr.push(titikCoordinatSplit);
+  }
+
+  const boardGame = shipDirection();
+
+  for (let i = 0; i < kumpulanTitikTembakArr.length; i++) {
+    var titikCoordinatX = kumpulanTitikTembakArr[i][0]
+    var titikCoordinatY = kumpulanTitikTembakArr[i][1]
+    if (boardGame[titikCoordinatX][titikCoordinatY] != '') {
+      boardGame[titikCoordinatX][titikCoordinatY] = '*'
+    } else {
+      boardGame[titikCoordinatX][titikCoordinatY] = '/'
+    }
+  }
+
+  console.log(boardGame)
+}
+
+tembakKapal([`${randomCoordinat()}${randomCoordinat()}`, `${randomCoordinat()}${randomCoordinat()}`]);
